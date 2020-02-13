a73af5ffbbcb19a9ea88531ecb338daa46bbefb2
Florian Tepelmann <ft@klapproth-koch.de>
2020-02-13 11:19:45 +0100

    Initial commit

---
+++ templates/homepage.php
@@ -0,0 +1,111 @@
+<div class="picture-grid">
+    <div class="picture-set">
+        <div class="picture" style="background-image: url('files/layout/homepage/H1.jpg');"></div>
+        <div class="picture" style="background-image: url('files/layout/homepage/1.jpg');"></div>
+    </div>
+    <div class="picture-set">
+        <div class="picture" style="background-image: url('files/layout/homepage/2.jpg');"></div>
+        <div class="picture" style="background-image: url('files/layout/homepage/H2.jpg');"></div>
+    </div>
+    <div class="picture-set">
+        <div class="picture" style="background-image: url('files/layout/homepage/H3.jpg');"></div>
+        <div class="picture" style="background-image: url('files/layout/homepage/3.jpg');"></div>
+    </div>
+    <div class="picture-set">
+        <div class="picture" style="background-image: url('files/layout/homepage/4.jpg');"></div>
+        <div class="picture" style="background-image: url('files/layout/homepage/H4.jpg');"></div>
+    </div>
+    <div class="picture-set">
+        <div class="picture" style="background-image: url('files/layout/homepage/H5.jpg');"></div>
+        <div class="picture" style="background-image: url('files/layout/homepage/5.jpg');"></div>
+    </div>
+    <div class="picture-set">
+        <div class="picture" style="background-image: url('files/layout/homepage/6.jpg');"></div>
+        <div class="picture" style="background-image: url('files/layout/homepage/H6.jpg');"></div>
+    </div>
+    <div class="picture-set">
+        <div class="picture" style="background-image: url('files/layout/homepage/H7.jpg');"></div>
+        <div class="picture" style="background-image: url('files/layout/homepage/7.jpg');"></div>
+    </div>
+    <div class="picture-set">
+        <div class="picture" style="background-image: url('files/layout/homepage/8.jpg');"></div>
+        <div class="picture" style="background-image: url('files/layout/homepage/H8.jpg');"></div>
+    </div>
+    <div class="picture-set">
+        <div class="picture" style="background-image: url('files/layout/homepage/H9.jpg');"></div>
+        <div class="picture" style="background-image: url('files/layout/homepage/9.jpg');"></div>
+    </div>
+    <div class="picture-set">
+        <div class="picture" style="background-image: url('files/layout/homepage/10.jpg');"></div>
+        <div class="picture" style="background-image: url('files/layout/homepage/H10.jpg');"></div>
+    </div>
+    <div class="picture-set">
+        <div class="picture" style="background-image: url('files/layout/homepage/H11.jpg');"></div>
+        <div class="picture" style="background-image: url('files/layout/homepage/11.jpg');"></div>
+    </div>
+    <div class="picture-set">
+        <div class="picture" style="background-image: url('files/layout/homepage/12.png');"></div>
+        <div class="picture" style="background-image: url('files/layout/homepage/H12.jpg');"></div>
+    </div>
+    <div class="picture-set">
+        <div class="picture" style="background-image: url('files/layout/homepage/H13.jpg');"></div>
+        <div class="picture" style="background-image: url('files/layout/homepage/13.jpg');"></div>
+    </div>
+    <div class="picture-set">
+        <div class="picture" style="background-image: url('files/layout/homepage/14.jpg');"></div>
+        <div class="picture" style="background-image: url('files/layout/homepage/H14.jpg');"></div>
+    </div>
+    <div class="picture-set">
+        <div class="picture" style="background-image: url('files/layout/homepage/H15.jpg');"></div>
+        <div class="picture" style="background-image: url('files/layout/homepage/15.jpg');"></div>
+    </div>
+    <div class="picture-set">
+        <div class="picture" style="background-image: url('files/layout/homepage/16.jpg');"></div>
+        <div class="picture" style="background-image: url('files/layout/homepage/H16.jpg');"></div>
+    </div>
+    <div class="picture-set">
+        <div class="picture" style="background-image: url('files/layout/homepage/H17.png');"></div>
+        <div class="picture" style="background-image: url('files/layout/homepage/17.jpg');"></div>
+    </div>
+    <div class="picture-set">
+        <div class="picture" style="background-image: url('files/layout/homepage/18.jpg');"></div>
+        <div class="picture" style="background-image: url('files/layout/homepage/H18.jpg');"></div>
+    </div>
+    <div class="picture-set">
+        <div class="picture" style="background-image: url('files/layout/homepage/H19.jpg');"></div>
+        <div class="picture" style="background-image: url('files/layout/homepage/19.jpg');"></div>
+    </div>
+    <div class="picture-set">
+        <div class="picture" style="background-image: url('files/layout/homepage/20.jpg');"></div>
+        <div class="picture" style="background-image: url('files/layout/homepage/H20.jpg');"></div>
+    </div>
+    <div class="picture-set">
+        <div class="picture" style="background-image: url('files/layout/homepage/H21.jpg');"></div>
+        <div class="picture" style="background-image: url('files/layout/homepage/21.jpg');"></div>
+    </div>
+    <div class="picture-set">
+        <div class="picture" style="background-image: url('files/layout/homepage/22.jpg');"></div>
+        <div class="picture" style="background-image: url('files/layout/homepage/H22.jpg');"></div>
+    </div>
+    <div class="picture-set">
+        <div class="picture" style="background-image: url('files/layout/homepage/H23.jpg');"></div>
+        <div class="picture" style="background-image: url('files/layout/homepage/23.jpg');"></div>
+    </div>
+    <div class="picture-set">
+        <div class="picture" style="background-image: url('files/layout/homepage/24.jpg');"></div>
+        <div class="picture" style="background-image: url('files/layout/homepage/H24.jpg');"></div>
+    </div>
+    <a href="about.html">
+        <div id="banner">
+            <div class="banner-inside">
+                <span>the world of the Others</span>
+            </div>
+        </div>
+    </a>
+    <a href="about.html">
+        <div id="banner-mobile">
+            <img src="files/layout/banner-mobile.PNG">
+        </div>
+    </a>
+    <div id="sticky-phantom"></div>
+</div>
